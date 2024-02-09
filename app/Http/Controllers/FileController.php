d16630cb40e694def243f98eb1f6d22500afbd26
Tiago Matos <tiago@tiagomatos.com>
2024-02-09 11:15:54 +1100

    Adicionando endpoints para CRUD de files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FileRequest;
+use App\Http\Resources\FileResource;
 use App\Models\File;
 use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
+    public function index()
+    {
+        $files = File::all();
+        return FileResource::collection($files);
+    }
+
     public function upload(FileRequest $request)
     {
         $input = $request->validated();
@@ -15,15 +22,23 @@ class FileController extends Controller
         $name = $file->getClientOriginalName();
         $path = $file->store('files', 'public');
 
-        File::query()->create([
+        $file = File::query()->create([
             'name' => $name,
             'path' => $path,
         ]);
+
+        return new FileResource($file);
     }
 
     public function download(File $file)
     {
         return Storage::disk('public')
             ->download($file->path);
+    }
+
+    public function delete(File $file)
+    {
+        Storage::disk('public')->delete($file->path);
+        $file->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('files', [FileController::class, 'index']);
 Route::post('files', [FileController::class, 'upload']);
 Route::get('files/{file}', [FileController::class, 'download']);
+Route::delete('files/{file}', [FileController::class, 'delete']);
